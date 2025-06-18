1047061270f2fb360375ddd077bf44307755e9ca
rgervacio0803 <ogturtel@yahoo.com>
2025-06-18 08:26:17 -0700

    Something wrong with webpage

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-template_uvu6g0h
-service_50v2zyo
-US3oQ39MhMt-kSSKz
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme" 
+    }
+    else {
+     document.body.classList.remove("dark-theme")   
+    }
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success'); 
+    loading.classlist += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_50v2zyo',
+            'template_uvu6g0h',
+            event.target,
+            'US3oQ39MhMt-kSSKz'
+        ).then(() => {
+           loading.classList.remove("modal__overlay--visible");
+           success.classlist += " modal__overlay--visible"; 
+        }).catch(() => {
+           loading.classList.remove("modal__overlay--visible"); 
+           alert(
+            "The email service is temporarily unavailable. Please contact me directly at rgervacio0803@gmail.com"
+            );
+        })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open");
+        
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
